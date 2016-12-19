ca967c5d73f0ced00458177ca883fa6f16e4fc12
Joey Lee <jleepianist@gmail.com>
2016-12-19 14:54:56 -0800

    Completed prompts

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
-};
+  Grub.call(this)
 
+  this.age = 5;
+  this.color = 'yellow';
+  //this.food is inherited from Grub 
+  //via the Grub call with Bee as the context
+  this.job = 'keep on growing';
+}
+
+//Bee.prototype.eat is inherited from Grub
+Bee.prototype = Object.create(Grub.prototype);
+//Point the Bee constructor back to itself
+Bee.prototype.constructor = Bee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,20 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  //this.color is inherited from Bee
+  this.honeyPot = 0;
 };
 
+//Bee.prototype.eat is inherited from Grub
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
